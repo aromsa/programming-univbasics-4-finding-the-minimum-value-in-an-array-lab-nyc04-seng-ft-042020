992a2e483e006edce37dd2c127a6d3a982f9deea
anna bateman <annacbateman@gmail.com>
2020-04-07 22:57:46 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -3,14 +3,12 @@ def find_min_value(array)
   
   #array.min
   
-  count = 0
-  min_value <= 2
-  while count < array.length do
-    if min_value > array[count]
-      min_value = array[count]
-    end
-    count += 1
-  end
-  min_value
+currentmin = nil
+while counter < array.size
+  if currentmin == nil ||
+    array[counter] < currentmin  
+    currentmin = array[counter]
 end
-
+counter += 1
+end
+return currentmin
